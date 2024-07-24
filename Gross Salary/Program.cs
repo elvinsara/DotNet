a9a9a93662c7483643c247ef0b2be7dd394f2fb0
elvinsara <elvinsara1999@gmail.com>
2024-07-24 13:00:00 +0530

    Calculate Gross Salary

---
+++ Gross Salary/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Gross_Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateEmployeeSalary();
+        }
+        public static void CalculateEmployeeSalary()
+        {
+            Console.WriteLine("Enter gross salary");
+            int salary = Convert.ToInt32(Console.ReadLine());
+            double grossSalary;
+            if (salary <= 10000)
+            {
+                grossSalary = salary + salary * 0.2 + salary * 0.8;
+            }
+            else if (salary <= 20000)
+            {
+                grossSalary = salary + salary * 0.25 + salary * 0.9;
+            }
+            else
+            {
+                grossSalary = salary + salary * 0.3 + salary * 0.95;
+            }
+            Console.WriteLine("Gross salary is : ", grossSalary);
+        }
+    }
+}
