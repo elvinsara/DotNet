8144cd0f0d9cd8f723b1263c46c9bc073ff506ff
elvinsara <elvinsara1999@gmail.com>
2024-07-24 13:02:39 +0530

    Add First Character to the beginning and end

---
+++ Add Characters to First And Last/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Add_Characters_to_First_And_Last
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddCharacter();
+        }
+        public static void AddCharacter()
+        {
+            Console.WriteLine("Enter a string");
+            string sentence = Console.ReadLine();
+            char firstCharacter = sentence[0];
+            Console.WriteLine(firstCharacter + sentence + firstCharacter);
+        }
+    }
+}
