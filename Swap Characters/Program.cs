4d2d5bbea221d91aebe5b9ea78e2821e61b7af96
elvinsara <elvinsara1999@gmail.com>
2024-07-24 13:05:32 +0530

    exchange the first and last characters in a given string and return the new string

---
+++ Swap Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Swap_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapCharacter();
+        }
+        public static void SwapCharacter()
+        {
+            Console.WriteLine("Enter a string");
+            string word = Console.ReadLine();
+            if (word.Length == 1)
+            {
+                Console.WriteLine(word);
+            }
+            else
+            {
+                char firstLetter = word[0];
+                char lastLetter = word[word.Length - 1];
+                string swappedWord = lastLetter + word.Substring(1, word.Length - 2) + firstLetter;
+                Console.WriteLine("swapped word is : " + swappedWord);
+            }
+        }
+    }
+}
