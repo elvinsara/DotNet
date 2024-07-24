371467398de07163a3b9a707e0f495422ce3811a
elvinsara <elvinsara1999@gmail.com>
2024-07-24 13:08:43 +0530

    3 characters added at both the front and back of the string

---
+++ Add Three Characters/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Add_Three_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddThreeCharacters();
+        }
+        public static void AddThreeCharacters()
+        {
+            Console.WriteLine("Enter a string : ");
+            string sentence = Console.ReadLine();
+            if (sentence.Length < 3)
+            {
+                Console.WriteLine(sentence + sentence + sentence);
+            }
+            else
+            {
+                Console.WriteLine(sentence.Substring(0, 3) + sentence + sentence.Substring(0, 3));
+            }
+        }
+    }
+}
