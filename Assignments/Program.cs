9085e882f95faa36e18a1b6f6105991d38b84868
elvinsara <elvinsara1999@gmail.com>
2024-07-24 12:57:07 +0530

    Calculate electricity unit

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CalculateUnit();
+        }
+        public static void CalculateUnit()
+        {
+            Console.WriteLine("Enter electricity unit");
+            int unit = Convert.ToInt32(Console.ReadLine());
+            double price = 1;
+            if (unit <= 50)
+            {
+                price = unit * 0.5;
+            }
+            else if (unit > 50 && unit <= 150)
+            {
+                price = unit * 0.75;
+            }
+            else if (unit > 150 && unit <= 250)
+            {
+                price = unit * 1.20;
+            }
+            else
+            {
+                price = unit + (unit * 0.2);
+            }
+            Console.WriteLine("Total price : "+price);
         }
     }
 }
