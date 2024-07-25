3a4524345a531ac3c82c97b5c614ebe00a0a7d81
elvinsara <elvinsara1999@gmail.com>
2024-07-25 20:27:33 +0530

    Check pallindrome string

---
+++ Pallindrome/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Pallindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string inputString = Console.ReadLine();
+            int inputStringLength = inputString.Length;
+          
+            bool notPallindrome = false;
+
+            if(inputStringLength !=1) {
+                for (int i = 0; i < inputString.Length; i++)
+                {
+
+                    if (inputString[i] != inputString[inputStringLength - 1])
+                    {
+                        //counter = counter + 1;
+                        notPallindrome = true;
+                        break;
+
+
+                    }
+
+
+                    inputStringLength--;
+
+                }
+            }
+           if (notPallindrome)
+            {
+                Console.WriteLine("Not a pallindrome");
+            }
+           else { Console.WriteLine("Pallindrome"); }
+            
+            
+        }
+    }
+}
