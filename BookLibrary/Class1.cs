24fe46508d8b07bfca76209a36e777d02e96bfef
elvinsara <elvinsara1999@gmail.com>
2024-08-03 19:28:09 +0530

    Book management

---
+++ BookLibrary/Class1.cs
@@ -0,0 +1,105 @@
+﻿namespace BookLibrary
+{
+    public class Book
+        {
+            // Properties
+            public int BookID { get; set; }
+            public string BookName { get; set; }
+            public string ISBNNo { get; set; }
+            public double Price { get; set; }
+            public string Publisher { get; set; }
+            public int NumberOfPages { get; set; }
+            public string Language { get; set; }
+            public string LoT { get; set; }
+            public string Summary { get; set; }
+
+            // Constructor
+            public Book(int id, string name, string isbn, double price, string publisher,
+                        int pages, string language = "English", string lot = "Technical", string summary = "")
+            {
+                BookID = id;
+                BookName = name;
+                ISBNNo = isbn;
+                Price = price;
+                Publisher = publisher;
+                NumberOfPages = pages;
+                Language = language;
+                LoT = lot;
+                Summary = summary;
+            }
+
+            //Get book details from user
+            public void GetDetails()
+            {
+
+                bool isInputValid = false;
+                while (!isInputValid)
+                {
+                    Console.WriteLine("Enter Book ID : ");
+                    string bookId = Console.ReadLine();
+
+                    bool isIdInt = int.TryParse(bookId, out int id);
+
+                    
+                    if (isIdInt && bookId.Length == 5)
+                    {
+                        BookID = id;
+                        isInputValid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Id is invalid.");
+                    }
+                }
+
+                Console.WriteLine("Enter Book Name: ");
+                BookName = Console.ReadLine();
+
+                Console.WriteLine("Enter ISBN Number: ");
+                ISBNNo = Console.ReadLine();
+
+                Console.WriteLine("Enter Price: ");
+                Price = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter Publisher: ");
+                Publisher = Console.ReadLine();
+
+                Console.WriteLine("Enter Number of Pages: ");
+                NumberOfPages = Convert.ToInt32(Console.ReadLine());
+
+               
+                Console.WriteLine("Enter Language - Default is English");
+                
+                Language = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(Language))
+                {
+                    Language = "English";
+                }
+
+
+            bool isLotValid;
+                    Console.WriteLine("Enter LoT (.NET, Java, IMS, V&V, BI, RDBMS): ");
+                    LoT = Console.ReadLine();
+
+                    // validating lot values
+                   
+                    if (LoT.ToLower() == ".net" || LoT.ToLower() == "java" || LoT.ToLower() == "ims" || LoT.ToLower() == "v&v" || LoT.ToLower() == "bi" || LoT.ToLower() == "rdbms")
+                    {
+                        isLotValid = true;
+                    }
+                    else
+                    {
+                //if no lot values is present, then default it is set to technical
+                   isLotValid = true;
+                    LoT = "Technical";
+                     
+                       
+                    }
+
+            Console.WriteLine("Enter Book summary:");
+            Summary = Console.ReadLine();
+        }
+        }
+    
+}
---
+++ BookManagement/Program.cs
@@ -0,0 +1,114 @@
+﻿using BookLibrary;
+
+namespace BookManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();            //Creating object of Book class
+
+            while (true)
+            {
+                Console.WriteLine("Book Management system:");
+                Console.WriteLine("1. Add a Book");
+                Console.WriteLine("2. Display all Books");
+                Console.WriteLine("3. Delete a Book by ID");
+                Console.WriteLine("4. Exit");
+
+
+
+                Console.Write("Enter your choice: ");
+
+                string choice;
+                choice = Console.ReadLine();
+                bool isChoiceDigit = int.TryParse(choice, out int switchChoice);
+                 
+
+                if (! isChoiceDigit)
+                {
+                    Console.WriteLine("Choice is invalid.");
+                    continue;
+                }
+
+                switch (switchChoice)
+                {
+                    case 1:
+                        AddBook(books);
+                        break;
+                    case 2:
+                        DisplayBooks(books);
+                        break;
+                    case 3:
+                        DeleteBook(books);
+                        break;
+                    case 4:
+                        Console.WriteLine("Exit");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
+
+        // Add Book
+        static void AddBook(List<Book> books)
+        {
+            // creating an object of book and assigning values to it
+            Book newBook = new Book(0, "", "", 0.0, "", 0);
+            //getting input from user
+            newBook.GetDetails();
+            //add new book to list collection
+            var IsSameBookIdPresent = books.Find(book => book.BookID == newBook.BookID);
+            if(IsSameBookIdPresent == null)
+            {
+                books.Add(newBook);
+                Console.WriteLine("Book added successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Book with same Id is present. Cannot add book");
+            }
+                                        
+            
+        }
+
+        //  display all books
+        static void DisplayBooks(List<Book> books)
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("No books available.");
+                return;
+            }
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Book ID: {book.BookID}, Name: {book.BookName}, ISBN_No: {book.ISBNNo}, Price: {book.Price}, " +
+                                  $"Publisher: {book.Publisher}, Pages: {book.NumberOfPages}, Language: {book.Language}, " +
+                                  $"LoT: {book.LoT}, Summary: {book.Summary}");
+            }
+        }
+
+        // delete a book by ID
+        static void DeleteBook(List<Book> books)
+        {
+            Console.WriteLine("Enter Book ID to delete: ");
+            int bookIdToDelete = Convert.ToInt32(Console.ReadLine());
+
+            Book bookToDelete = books.Find(b => b.BookID == bookIdToDelete);        
+            if (bookToDelete != null)
+            {
+                books.Remove(bookToDelete);
+                Console.WriteLine("Book deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Book not found.");
+            }
+        }
+    }
+    
+}
