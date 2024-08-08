6cc84a7b452329e99be48cfbc3b69bcae441b9e6
elvinsara <elvinsara1999@gmail.com>
2024-08-08 19:35:50 +0530

    Pattern

---
+++ PatternMatching/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PatternMatching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern1 = "a*b";
+            string pattern2 = "a+b";
+            Regex reg = new Regex(pattern2);
+            Match match = reg.Match("ahhbcd");
+            if(match.Success )
+            {
+                Console.WriteLine("Success");
+            }
+            else { Console.WriteLine("Fail"); }
+
+
+        }
+    }
+}
