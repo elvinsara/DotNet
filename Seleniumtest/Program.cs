29cd1258d794b2b0f049d046f75ada8395fd7b1e
elvinsara <elvinsara1999@gmail.com>
2024-09-03 15:10:38 +0530

    Selenium testing

---
+++ Seleniumtest/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Seleniumtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");*/
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            LoginPage _loginPage = new LoginPage(driver);
+            _loginPage.Login("standard_user", "secret_sauce");
+
+        }
+    }
+}
